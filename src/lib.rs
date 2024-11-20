4c73ba17bf27ec2d849b3200fc4d08864f42253d
keithyin <yinpenghhz@hotmail.com>
2024-11-20 23:11:58 +0800

    first

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub mod gsbam;
+pub mod fastx_reader;
+pub mod pbar;
+pub mod samtools;
+pub mod utils;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
