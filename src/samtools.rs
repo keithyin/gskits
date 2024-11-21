a70506421d68bbc0f9ad29ae19ee91f0672fc2cf
roc <peng.yin@geneus-tech.com>
2024-11-21 09:27:39 +0000

    sort by tag

--- src/samtools.rs
+++ src/samtools.rs
@@ -63,3 +63,35 @@ pub fn sort_by_coordinates(bam_file: &str, threads: Option<usize>) {
         );
     }
 }
+
+pub fn sort_by_tag(bam_file: &str, tag: &str) -> String {
+    let out_bam = format!(
+        "{}.sort_by_{}.bam",
+        bam_file.rsplit_once(".").unwrap().0,
+        tag
+    );
+
+    let mut cmd = Command::new("samtools");
+    cmd.args([
+        "sort",
+        "-n",
+        "-t",
+        tag,
+        "-o",
+        out_bam.as_str(),
+        "-@",
+        "40",
+        bam_file,
+    ]);
+
+    let oup = cmd.output().expect(&format!("sort {} error", bam_file));
+    if !oup.status.success() {
+        panic!(
+            "sort {} error. {}",
+            bam_file,
+            String::from_utf8(oup.stderr).unwrap()
+        );
+    }
+
+    out_bam
+}
