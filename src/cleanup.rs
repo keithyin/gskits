842466e236ba81a2a250d240b28499f0b9210c1d
roc <peng.yin@geneus-tech.com>
2025-04-10 09:25:45 +0000

    auto clean file

--- src/cleanup.rs
+++ src/cleanup.rs
@@ -1,5 +1,7 @@
-use std::{fs, path};
+use std::{fs, ops::Deref, path};
 
+
+/// if the object is dropped, the file will be deleted
 #[derive(Debug)]
 pub struct AutoCleanFile {
     filename: String,
@@ -11,9 +13,33 @@ impl AutoCleanFile {
     }
 }
 
+impl Deref for AutoCleanFile {
+    type Target = String;
+
+    fn deref(&self) -> &Self::Target {
+        &self.filename
+    }
+}
+
 impl From<String> for AutoCleanFile {
     fn from(value: String) -> Self {
         Self { filename: value }
+    }
+}
+
+impl From<&String> for AutoCleanFile {
+    fn from(value: &String) -> Self {
+        Self {
+            filename: value.to_string(),
+        }
+    }
+}
+
+impl From<&str> for AutoCleanFile {
+    fn from(value: &str) -> Self {
+        Self {
+            filename: value.to_string(),
+        }
     }
 }
 
