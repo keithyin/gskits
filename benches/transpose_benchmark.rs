b30580e26b2a665cb4250808dab459980ce33b2c
keithyin <yinpenghhz@hotmail.com>
2025-09-04 23:45:59 +0800

    benckmark

--- benches/transpose_benchmark.rs
+++ benches/transpose_benchmark.rs
@@ -9,20 +9,32 @@ fn transpose_benchmark(c: &mut Criterion) {
 }
 
 fn transpose_blocked_benchmark(c: &mut Criterion) {
-    let values = (0..(33750_usize * 88704))
+    let values = (0..(3375_usize * 8870))
         .into_iter()
         .map(|v| (v % 256) as u8)
         .collect();
-    c.bench_function("transpose_block", |b| {
-        b.iter(|| {
-            transpose::transpose_blocked(
-                black_box(&values),
-                black_box(33750),
-                black_box(88704),
-                black_box(None),
-            )
-        })
-    });
+
+    let mut group = c.benchmark_group("transpose_blocked_benchmark");
+    for block_size in [
+        None,
+        Some(8),
+        Some(16),
+        Some(32),
+        Some(64),
+        Some(128),
+        Some(256),
+    ] {
+        group.bench_function(format!("{:?}", block_size), |b| {
+            b.iter(|| {
+                transpose::transpose_blocked(
+                    black_box(&values),
+                    black_box(3375),
+                    black_box(8870),
+                    black_box(block_size),
+                )
+            })
+        });
+    }
 }
 
 criterion_group!(benches, transpose_benchmark, transpose_blocked_benchmark);
